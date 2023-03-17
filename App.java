0d99c7cef06255025fb071d2d6edefb74dd9cfa7
Pedro B B Fonseca <pedrofonseca447@gmail.com>
2023-03-17 12:35:58 +0000

    sla

--- App.java
+++ App.java
@@ -15,27 +15,28 @@ public class App{
  
      public  App (){
           Scanner in = new Scanner(System.in);
-
+          String estado = in.next();
+          String cidade = in.next();
+          String logradouro = in. next();
          // System.out.println("digite um cep valido");
          // String cep = in.next();
          
          // String url = "https://viacep.com.br/ws/01001000/json/";
         // String url = "https://viacep.com.br/ws/"+cep+"/json/";
          //cidade com nome composto ta dando problema 
-          String urn = "https://viacep.com.br/ws/RS/Gravatai/Panorama/json/";
+          //String urn = "https://viacep.com.br/ws/RS/Gravatai/Panorama/json/";
+          String urn = "https://viacep.com.br/ws/"+estado+"/"+cidade+"/"+logradouro+"/json/";
 
     try { HttpClient httpClient = HttpClient.newBuilder() .connectTimeout(Duration.of(1, MINUTES))
          .build();
-
-        var httpRequest = HttpRequest.newBuilder()
-     
+       var httpRequest = HttpRequest.newBuilder()
      .GET()
      .uri(URI.create(urn))
      
      .build();
-
-        var httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-          System.out.println(httpResponse.body()); 
+      var httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+          System.out.println(httpResponse.body());
+        
         } catch (IOException | InterruptedException e) { 
           e.printStackTrace();
           throw new RuntimeException(e.getMessage());
